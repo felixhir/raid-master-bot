bf4933cbd3688863872677808fc3f5401c5e5e8a
felixhir <felix.hirschel@outlook.de>
2021-01-31 14:19:50 +0100

    playerlist now properly extends linkedlist, got a testsuite

--- src/main/java/players/PlayerList.java
+++ src/main/java/players/PlayerList.java
@@ -1,31 +1,13 @@
 package players;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class PlayerList extends LinkedList<Player> {
 
-    private final LinkedList<Player> players;
-
-    public PlayerList(){
-        players = new LinkedList<>();
-    }
-
     public boolean containsPlayerById(String id){
-        for (Player player : players) {
+        for (Player player : this) {
             if (player.getId().equals(id)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean containsName(String name){
-        for (Player player : players) {
-            if (player.getRealName().equals(name)) {
                 return true;
             }
         }
@@ -34,19 +16,19 @@ public class PlayerList extends LinkedList<Player> {
 
     @Override
     public boolean add(Player player){
-        if(this.containsPlayerById(player)) {
+        if(this.containsPlayerById(player.getId())) {
             Player editPlayer = getPlayerById(player.getId());
             editPlayer.addAttacks(player.getAttacks());
             editPlayer.setRealName(player.getRealName());
             editPlayer.addDamage(player.getDamage());
             return true;
         } else {
-            return players.add(player);
+            return super.add(player);
         }
     }
 
     public Player getPlayerById(String id){
-        for(Player p: players){
+        for(Player p: this){
             if(p.getId().equals(id)){
                 return p;
             }
@@ -55,7 +37,7 @@ public class PlayerList extends LinkedList<Player> {
     }
 
     public Player getPlayerByName(String name){
-        for(Player p: players){
+        for(Player p: this){
             if(p.getRealName().equals(name)){
                 return p;
             }
@@ -64,69 +46,32 @@ public class PlayerList extends LinkedList<Player> {
     }
 
     @Override
-    public Player get(int index){
-        return players.get(index);
-    }
-
-    @Override
-    public int size(){
-        return players.size();
-    }
-
-    public LinkedList<Player> getList(){
-        return this.players;
-    }
-
-    @Override
     public String toString(){
-        StringBuilder returnString = new StringBuilder();
-        for(Player p: players){
-            returnString.append(p.toString()).append("\n");
+        StringBuilder returnString = new StringBuilder("List of players:");
+        for(Player p: this){
+            returnString.append(" '").append(p.getRealName()).append("',");
         }
-        return returnString.toString();
-    }
-
-    /**
-     * Implements a subList() for PlayerList
-     * @param start index of first player
-     * @param end index of last player (inclusive)
-     * @return list of players from index start to end
-     */
-    @Override
-    public @NotNull PlayerList subList(int start, int end){
-        PlayerList list = new PlayerList();
-        for(int i = start; i < end+1; i++){
-            list.add(players.get(i));
-        }
-        return list;
+        return returnString.substring(0,returnString.length()-1);
     }
 
     /**
      * Determines the 5 players with the highest damage per attack (dpa)
      * @return PlayerList with 5 highest ranked players
      */
-    public PlayerList getTopPlayers(){
-        PlayerList list = new PlayerList();
-        for(Player p: this.getList()){
+    public List<Player> getTopPlayers(){
+        LinkedList<Player> list = new LinkedList<>();
+        for(Player p: this){
             for(int i = 0; i < list.size(); i++){
                 if(p.getDpa() > list.get(i).getDpa()){
-                    list.getList().add(i, p);
+                    list.add(i, p);
                     break;
                 }
             }
-            if(!list.getList().contains(p)){
+            if(!list.contains(p)){
                 list.add(p);
             }
         }
-        return list.subList(0,4);
+        return list.subList(0,Math.min(5, list.size()));
     }
 
-    private boolean containsPlayerById(Player player) {
-        for(Player p: players) {
-            if(p.getId().equals(player.getId())) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
---
+++ src/test/java/players/PlayerListTest.java
@@ -0,0 +1,112 @@
+package players;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerListTest {
+
+    private PlayerList list;
+    private Player playerOne;
+    private Player playerTwo;
+
+    @BeforeEach
+    void setUp() {
+        list = new PlayerList();
+        playerOne = new Player(Arrays.toString("Chad Thundercock".getBytes(StandardCharsets.UTF_8)),
+                "abcdefg",
+                100,
+                666000);
+        playerTwo = new Player(Arrays.toString("Virgin".getBytes(StandardCharsets.UTF_8)),
+                "000aaa",
+                1,
+                0);
+    }
+
+    @Test
+    void add_addsPlayers() {
+        boolean result1 = list.add(playerOne);
+        boolean result2 = list.add(playerTwo);
+
+        assertTrue(result1);
+        assertTrue(result2);
+    }
+
+    @Test
+    void add_doesntAddPlayerTwice() {
+        list.add(playerOne);
+        list.add(playerOne);
+        int result = list.size();
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void add_updatesPlayer() {
+        list.add(playerOne);
+        list.add(new Player(Arrays.toString("Test".getBytes(StandardCharsets.UTF_8)), "abcdefg", 1,1));
+        Player result = list.get(0);
+
+        assertEquals("Test", result.getRealName());
+        assertEquals("abcdefg", result.getId());
+        assertEquals(101, result.getAttacks());
+        assertEquals(666001, result.getDamage());
+    }
+
+    @Test
+    void toString_returnsNames() {
+        list.add(playerOne);
+        list.add(playerTwo);
+        String result = list.toString();
+
+        assertEquals("List of players: 'Chad Thundercock', 'Virgin'", result);
+    }
+
+    @Test
+    void getPlayerById_returnsPlayer() {
+        list.add(playerOne);
+        Player result = list.getPlayerById("abcdefg");
+
+        assertEquals(playerOne, result);
+    }
+
+    @Test
+    void getPlayerById_returnsNull() {
+        list.add(playerOne);
+        Player result = list.getPlayerById("way too long of an id");
+
+        assertNull(result);
+    }
+
+    @Test
+    void getPlayerByName_returnsPlayer() {
+        list.add(playerTwo);
+        String result = list.getPlayerByName("Virgin").getRealName();
+
+        assertEquals(playerTwo.getRealName(), result);
+    }
+
+    @Test
+    void getPlayerByName_returnsNull() {
+        list.add(playerTwo);
+        list.add(playerOne);
+        Player result = list.getPlayerByName("This name does not exist");
+
+        assertNull(result);
+    }
+
+    @Test
+    void getTopPlayers_givesTopTwo() {
+        list.add(playerTwo);
+        list.add(playerOne);
+        String result = list.getTopPlayers().get(0).getRealName();
+
+        assertEquals("Chad Thundercock", result);
+    }
+}
